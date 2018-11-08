853a9dad4fa71cb0f7f98775b60ccfa24809f2c2
Liam Wilbraham <l.wilbraham@ucl.ac.uk>
2018-11-08 23:16:36 +0000

    added train test split

--- mlp-general/mlp.py
+++ mlp-general/mlp.py
@@ -16,12 +16,20 @@ class MLP:
         self.n_output = n_output
         self.layers = layers
 
-    def load_data(self, filepath, smiles_col, y_cols, sep=' '):
-        data = pd.read_csv(filepath, sep=sep).head(20000) ####### remove head eventually
+
+    def load_data(self,
+                  filepath,
+                  smiles_col,
+                  y_cols,
+                  sep=' '):
+
+        data = pd.read_csv(filepath, sep=sep).head(2000) ####### remove head eventually
         self.smiles = data[smiles_col]
         self.y = np.column_stack((data[y].values for y in y_cols))
 
-    def fingerprint(self):
+
+    def fingerprint(self, test_frac=None):
+
         def morgan(smi):
             mol = Chem.MolFromSmiles(smi)
             fp = AllChem.GetMorganFingerprintAsBitVect(mol, 2, nBits=self.n_input)
@@ -31,20 +39,32 @@ class MLP:
         for index, smi in enumerate(self.smiles):
             x[index] = morgan(smi)
 
-        if train_frac is not None:
-            self.x_train, self.x_test, self.y_train, self.y_test =
-                train_test_split(x, self.y, test_size=test_frac)
+        if test_frac is not None:
+            self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(x, self.y, test_size=test_frac)
         else:
             self.x_train = x
+            self.y_train= self.y
+
 
     def build_network(self, activation=tf.nn.relu):
+
         network = []
         for i in range(0, len(self.layers)):
                 network.append(tf.keras.layers.Dense(self.layers[i], activation=activation))
         network.append(tf.keras.layers.Dense(self.n_output, activation=activation))
         self.model = tf.keras.Sequential(network)
 
-    def train(self, learning_rate=0.1, epochs=10, batch_size=1, loss='mean_absolute_error', optimizer='sgd'):
+
+    def train(self,
+              learning_rate=0.1,
+              epochs=10,
+              batch_size=1,
+              loss='mean_absolute_error',
+              optimizer='sgd'):
+
         self.model.compile(optimizer=optimizer,loss=loss)
         self.model.fit(self.x_train, self.y_train, epochs=epochs)
         self.model.summary()
+
+    def evaluate():
+        pass
